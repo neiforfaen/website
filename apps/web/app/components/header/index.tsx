4a186c568d097e994d90076ec2c252c2ebdd9d7d
Kaiden Riley <kaiden.riley@pleo.io>
2025-04-26 15:00:57 +0100

    feat: add basic homepage

--- apps/web/app/components/header/index.tsx
+++ apps/web/app/components/header/index.tsx
@@ -3,10 +3,8 @@ import { ThemeSwitcher } from '@nff/design-system/components/theme-switcher'
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 left-0 z-40 w-full bg-background">
-      <div className="flex w-full flex-row items-center justify-center gap-3 p-[4.2rem]">
-        <ThemeSwitcher />
-      </div>
+    <header className="flex w-full flex-row items-center justify-center p-[4.2rem]">
+      <ThemeSwitcher />
     </header>
   )
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,33 +1,56 @@
-import { Button } from '@nff/design-system/components/ui/button'
-import { MoveRight, PhoneCall } from 'lucide-react'
-import Link from 'next/link'
+import { TickingClock } from '@nff/design-system/components/ticking-clock'
+import { BriefcaseBusiness, Clock, MapPin } from 'lucide-react'
 
 const Home = () => {
   return (
-    <div className="w-full">
-      <div className="container mx-auto">
-        <div className="flex flex-col items-center justify-center gap-8 py-20 lg:py-40">
-          <div className="flex flex-col gap-4">
-            <h1 className="max-w-2xl text-center font-regular text-5xl tracking-tighter md:text-7xl">
-              {'Title'}
-            </h1>
-            <p className="max-w-2xl text-center text-lg text-muted-foreground leading-relaxed tracking-tight md:text-xl">
-              {
-                'lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.'
-              }
-            </p>
-          </div>
-          <div className="flex flex-row gap-3">
-            <Button size="lg" className="gap-4" variant="outline" asChild>
-              <Link href="/contact">
-                Get in touch <PhoneCall className="h-4 w-4" />
-              </Link>
-            </Button>
-            <Button size="lg" className="gap-4" asChild>
-              <Link href={'/'}>
-                Sign up <MoveRight className="h-4 w-4" />
-              </Link>
-            </Button>
+    <div
+      id="portfolio-container"
+      className="mx-auto my-0 mt-[70px] animate-[fadeBlur_2.5s_ease-in-out_forwards] xl:my-0 xl:w-[1200px]"
+    >
+      <div
+        id="intro-section"
+        className="my-[72px] xl:mx-[72px] xl:flex xl:justify-between 2xl:mx-[72px] xl:[align-items:initial]"
+      >
+        <div
+          id="intro-words"
+          className="mx-auto my-0 mb-18 w-[330px] xl:flex xl:flex-col xl:justify-between xl:[margin:initial] xl:[padding-left:initial] xl:[width:initial]"
+        >
+          <h1 className="color-white my-[30px] font-normal font-sans text-[44px] xl:m-0 xl:w-[800px] xl:text-[106px] xl:leading-[124px]">
+            I enjoy creating{' '}
+            <span className="-skew-x-[16deg] inline-block font-semibold text-[44px] text-white leading-tight tracking-tighter xl:text-[106px] xl:leading-[124px]">
+              digital
+            </span>{' '}
+            products.
+          </h1>
+          <div id="hasque-status" className="text-white xl:mt-[72px] xl:flex">
+            <div
+              id="previous-experience"
+              className="w-[300px] xl:[width:initial]"
+            >
+              <BriefcaseBusiness className="mb-3 h-[20px] w-auto" />
+              <p className="m-0 font-bold">Current Experience</p>
+              <p id="status-description" className="m-0 mt-1 text-[#7c7c83]">
+                Associate Frontend Engineer II @ Pleo.
+              </p>
+            </div>
+            <div
+              id="location"
+              className="mt-11 xl:ml-[72px] xl:[margin-top:initial]"
+            >
+              <MapPin className="mb-3 h-[20px] w-auto" />
+              <p className="m-0 font-bold">Location</p>
+              <p id="status-description" className="m-0 mt-1 text-[#7c7c83]">
+                Berlin
+              </p>
+            </div>
+            <div
+              id="location"
+              className="mt-11 hidden xl:ml-[72px] xl:block xl:[margin-top:initial]"
+            >
+              <Clock className="mb-3 h-[20px] w-auto" />
+              <p className="m-0 font-bold">Local Time</p>
+              <TickingClock />
+            </div>
           </div>
         </div>
       </div>
