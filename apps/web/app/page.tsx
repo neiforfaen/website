03d2e1700adb0e73c4f1281765783360b0098c34
Kaiden Riley <kaiden.riley@pleo.io>
2025-04-26 15:17:08 +0100

    chore: remove unused ids

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,18 +3,9 @@ import { BriefcaseBusiness, Clock, MapPin } from 'lucide-react'
 
 const Home = () => {
   return (
-    <div
-      id="portfolio-container"
-      className="mx-auto my-0 mt-[70px] 3xl:w-[1400px] animate-[fadeBlur_2.5s_ease-in-out_forwards] xl:my-0 xl:w-[1200px]"
-    >
-      <div
-        id="intro-section"
-        className="my-[72px] xl:mx-[72px] xl:flex xl:justify-between 2xl:mx-[72px] xl:[align-items:initial]"
-      >
-        <div
-          id="intro-words"
-          className="mx-auto my-0 mb-18 w-[330px] xl:flex xl:flex-col xl:justify-between xl:[margin:initial] xl:[padding-left:initial] xl:[width:initial]"
-        >
+    <div className="mx-auto my-0 mt-[70px] 3xl:w-[1400px] animate-[fadeBlur_2.5s_ease-in-out_forwards] xl:my-0 xl:w-[1200px]">
+      <div className="my-[72px] xl:mx-[72px] xl:flex xl:justify-between 2xl:mx-[72px] xl:[align-items:initial]">
+        <div className="mx-auto my-0 mb-18 w-[330px] xl:flex xl:flex-col xl:justify-between xl:[margin:initial] xl:[padding-left:initial] xl:[width:initial]">
           <h1 className="color-white my-[30px] font-normal font-sans text-[44px] xl:m-0 xl:w-[800px] xl:text-[106px] xl:leading-[124px]">
             I enjoy creating{' '}
             <span className="-skew-x-[16deg] inline-block font-semibold text-[44px] text-white leading-tight tracking-tighter xl:text-[106px] xl:leading-[124px]">
@@ -22,31 +13,20 @@ const Home = () => {
             </span>{' '}
             products.
           </h1>
-          <div id="hasque-status" className="text-white xl:mt-[72px] xl:flex">
-            <div
-              id="previous-experience"
-              className="w-[300px] xl:[width:initial]"
-            >
+          <div className="text-white xl:mt-[72px] xl:flex">
+            <div className="w-[300px] xl:[width:initial]">
               <BriefcaseBusiness className="mb-3 h-[20px] w-auto" />
               <p className="m-0 font-bold">Current Experience</p>
-              <p id="status-description" className="m-0 mt-1 text-[#7c7c83]">
+              <p className="m-0 mt-1 text-[#7c7c83]">
                 Associate Frontend Engineer II @ Pleo.
               </p>
             </div>
-            <div
-              id="location"
-              className="mt-11 xl:ml-[72px] xl:[margin-top:initial]"
-            >
+            <div className="mt-11 xl:ml-[72px] xl:[margin-top:initial]">
               <MapPin className="mb-3 h-[20px] w-auto" />
               <p className="m-0 font-bold">Location</p>
-              <p id="status-description" className="m-0 mt-1 text-[#7c7c83]">
-                Berlin
-              </p>
+              <p className="m-0 mt-1 text-[#7c7c83]">Berlin</p>
             </div>
-            <div
-              id="location"
-              className="mt-11 hidden xl:ml-[72px] xl:block xl:[margin-top:initial]"
-            >
+            <div className="mt-11 hidden xl:ml-[72px] xl:block xl:[margin-top:initial]">
               <Clock className="mb-3 h-[20px] w-auto" />
               <p className="m-0 font-bold">Local Time</p>
               <TickingClock />
