9dfaf8b2e53e946ef6874341b96c7dc303c66efd
Kaiden Riley <kaiden.riley@pleo.io>
2025-04-27 17:39:11 +0100

    chore: swap useInterval for useTimeout

--- packages/design-system/components/ticking-clock.tsx
+++ packages/design-system/components/ticking-clock.tsx
@@ -3,33 +3,29 @@
 import { useEffect, useState } from 'react'
 
 export const TickingClock = () => {
-  const getTime = (): string => {
-    const now = new Date()
-
-    const opts: Intl.DateTimeFormatOptions = {
+  const getTime = (): string =>
+    new Date().toLocaleTimeString('en-US', {
       timeZone: 'Europe/Berlin',
       hour: '2-digit',
       minute: '2-digit',
       hour12: false,
-    }
+    })
 
-    return now.toLocaleTimeString('en-US', opts)
-  }
-
-  const [time, setTime] = useState('')
+  const [time, setTime] = useState(getTime)
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: getTime changes on every render
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(getTime())
-    }, 1000)
+    let timeoutId: NodeJS.Timeout
 
-    return () => clearInterval(interval)
+    const tick = () => {
+      setTime(getTime())
+      timeoutId = setTimeout(tick, 1000 - (Date.now() % 1000))
+    }
+
+    timeoutId = setTimeout(tick, 1000 - (Date.now() % 1000))
+
+    return () => clearTimeout(timeoutId)
   }, [])
 
-  return (
-    <p id="status-description" className="m-0 mt-1 text-[#7c7c83]">
-      {time || '04:20'}
-    </p>
-  )
+  return <p className="m-0 mt-1 text-[#7c7c83]">{time || '00:00'}</p>
 }
