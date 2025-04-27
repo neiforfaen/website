b50570d445e949a1508bb079348752b60fcc3e82
Kaiden Riley <kaiden.riley@pleo.io>
2025-04-27 13:06:17 +0100

    chore: update footer text

--- apps/web/app/components/footer/index.tsx
+++ apps/web/app/components/footer/index.tsx
@@ -1,9 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="mx-[72px] flex items-center justify-end py-4">
-      <p className="text-[#7c7c83]/30 text-xs">
-        built using next.js and tailwind
-      </p>
+      <p className="text-[#7c7c83]/30 text-xs">built with next.js + tailwind</p>
     </footer>
   )
 }
